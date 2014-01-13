fe0f66b1b0b29697f3ed01f64e0c644ca1268a7f
段梦娜 <duanmengna@danmengnatekiMac.local>
2014-01-13 11:30:07 +0800

    Initial Commit

---
+++ Myiostest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Myiostest
+//
+//  Created by 段梦娜 on 14-1-13.
+//  Copyright (c) 2014年 nana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
